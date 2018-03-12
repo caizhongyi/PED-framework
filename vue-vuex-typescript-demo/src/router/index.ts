e11a002e5da34c1410cc024e2092cec63537c9ab
cai <274142680@qq.com>
2018-03-12 14:44:27 +0800

    1

--- vue-vuex-typescript-demo/src/router/index.ts
+++ vue-vuex-typescript-demo/src/router/index.ts
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld.vue'
 import ShoppingCart from '@/components/ShoppingCart.vue'
+import NotFoundComponent from '@/components/NotFoundComponent.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+      { path: '*', component: NotFoundComponent },
     {
       path: '/',
       name: 'Hello',
