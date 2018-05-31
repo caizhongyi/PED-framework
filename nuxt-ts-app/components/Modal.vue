3165457ac76dd11b2cd91fa1de8e0f4ef809d67a
cai <274142680@qq.com>
2018-05-31 18:59:33 +0800

    1

---
+++ nuxt-ts-app/components/Modal.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="card">
+    <h2> </h2>
+    <img :src="'https://robohash.org/' + person.first_name + '_' + person.last_name" />
+  </div>
+</template>
+<script lang="ts">
+import {
+  Component,
+  Prop,
+  Vue,
+  Watch,
+  Model
+} from "nuxt-property-decorator"
+import { Getter, Action } from 'vuex-class'
+
+@Component({})
+export default class Modal extends Vue {
+  /* @Prop({ default: '' })
+ @Getter('checkoutStatus') checkoutStatus: CheckoutStatus
+ text: string
+
+ // store
+ @userState userId
+
+ // computed
+ get name (): boolean {
+   return this.title + this.text
+ }
+
+ // watch
+ @Watch('text')
+ onChangeText () { }*/
+
+  /*@Prop()
+  value: boolean = false;
+
+  @Watch('value')
+  onChangeValue( val ) {
+    this.visible = val;
+  }*/
+
+  @Model()
+  visible:boolean = false;
+
+  @Watch('visible')
+  onChangeVisible( val ) {
+    this.$emit('input', val);
+  }
+  mounted() {
+  }
+}
+</script>
+<style scoped>
+.card {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana,
+    sans-serif;
+  padding: 1rem;
+  margin: 0.25rem;
+  border: 0.25rem solid gainsboro;
+}
+</style>
