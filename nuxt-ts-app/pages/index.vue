49ea38d6440143d7e80d8db8a9ec514f29cfd9d9
cai <274142680@qq.com>
2018-06-07 09:50:08 +0800

    11

--- nuxt-ts-app/pages/index.vue
+++ nuxt-ts-app/pages/index.vue
@@ -1,10 +1,13 @@
 <template>
+
   <section>
+    <button type="button" @click="goto()">link</button>
     <h1 class="header">Nuxt TypeScript Starter</h1>
     <div class="cards">
       <Card v-for="person in people" :key="person.id" :person="person"></Card>
     </div>
   </section>
+
 </template>
 
 <script lang="ts">
@@ -24,6 +27,12 @@ export default class extends Vue {
   scrollToTop = true
   @State people
   //layout = 'default'
+  goto(){
+    this.$router.push({
+      name:'page1',
+      query : { id : "1" , value : "0" }
+    })
+  }
 }
 </script>
 <style scoped>
---
+++ nuxt-ts-app/pages/page1/index.vue
@@ -0,0 +1,33 @@
+<template>
+   <!-- {{data}}-->
+  <!--  <label v-for="o in data">
+        <input type="radio" v-model="o.active">
+    </label>-->
+
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import Component from 'vue-class-component'
+
+  @Component
+  export default class Page1 extends Vue {
+     data = [
+       { active : true , value : '1'},
+       { active : false , value : '1'},
+       { active : true , value : '1'},
+       { active : false , value : '1'},
+       { active : true , value : '1'},
+       { active : true , value : '1'}
+     ]
+    mounted(){
+        console.log(this.data)
+        console.log( this.$route )
+        console.log( this.$router )
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
